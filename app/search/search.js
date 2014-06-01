6afbbded2ba4cffd317f10b61a53aa1aa71c6951
Peter Carroll <peter@eringal.com>
2014-06-01 09:36:56 -0500

    Added French

--- app/search/search.js
+++ app/search/search.js
@@ -14,7 +14,7 @@ angular.module('pjcWordMultiSearch.search', [
 .controller('SearchPageController', function ($scope, $sce) {
 
     var getRhinoSpikeLink = function(lang, search) {
-      var langCode = {'eng': '1', 'epo': '22', 'spa': '2'};
+      var langCode = {'eng': '1', 'epo': '22', 'fra': '6', 'spa': '2'};
       var url = 'https://rhinospike.com/search/?language={lang}&q={search}'
         .replace('{search}', encodeURIComponent(search))
         .replace('{lang}', langCode[lang]);      
@@ -22,7 +22,7 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getForvoLink = function(lang, search) {
-      var langCode = {'eng': 'en', 'epo': 'eo', 'spa': 'es'};
+      var langCode = {'eng': 'en', 'epo': 'eo', 'fra': 'fr', 'spa': 'es'};
       var url = 'http://www.forvo.com/search-{lang}/{search}/'
         .replace('{search}', encodeURIComponent(search))
         .replace('{lang}', langCode[lang]);
@@ -30,13 +30,13 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getTatoebaLink = function(lang, search) {
-      var url = 'http://tatoeba.org/{lang}/sentences/search?query={search}&from={lang}&to=und'.replace('{search}', encodeURIComponent(search));
+      var url = 'http://tatoeba.org/eng/sentences/search?query={search}&from={lang}&to=und'.replace('{search}', encodeURIComponent(search));
       url=url.replace(/\{lang\}/g, lang);
       return $sce.trustAsResourceUrl(url);
     };
 
     var getLingueeLink = function(lang, search) {
-      var langCode = {'spa': 'english-spanish'};
+      var langCode = {'fra': 'english-french', 'spa': 'english-spanish'};
       var url = 'http://www.linguee.com/{lang}/search?source=auto&query={search}'
         .replace('{search}', encodeURIComponent(search))
         .replace('{lang}', langCode[lang]);
@@ -44,7 +44,7 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getGoogleImagesLink = function(lang, search) {
-      var langCode = {'eng': 'com', 'spa': 'es'};
+      var langCode = {'eng': 'com', 'fra': 'fr', 'spa': 'es'};
       var url = 'ba-simple-proxy.php?mode=native&send_cookies=0&url=https%3A%2F%2Fwww.google.{lang}%2Fsearch%3Fq%3D{search}%26tbm%3Disch%26sout%3D1%26biw%3D1266%26bih%3D1442%26dpr%3D1'
         .replace('{search}', encodeURIComponent(search))
         .replace('{lang}', langCode[lang]);
@@ -52,7 +52,7 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getGoogleTranslateLink = function(lang, search) {
-      var langCode = {'epo': 'eo', 'spa': 'es'};
+      var langCode = {'epo': 'eo', 'fra': 'fr', 'spa': 'es'};
       var url = 'ba-simple-proxy.php?mode=native&send_cookies=0&url=http%3A%2F%2Ftranslate.google.com%2F%3Fie%3DUTF-8%26sl%3D{lang}%26tl%3Den%26text%3D{search}'
         .replace('{search}', encodeURIComponent(search));
       return $sce.trustAsResourceUrl(url);
@@ -62,6 +62,7 @@ angular.module('pjcWordMultiSearch.search', [
       var url = '';
       switch(lang){
         case 'epo': url='http://lernu.net/cgi-bin/serchi.pl?delingvo=eo&allingvo=en&modelo=' + encodeURIComponent(search); break;
+        case 'fra': url='http://www.wordreference.com/fren/' + encodeURIComponent(search); break;
         case 'spa': url='http://www.wordreference.com/es/en/translation.asp?spen=' + encodeURIComponent(search); break;
       }
       return $sce.trustAsResourceUrl(url);
@@ -72,6 +73,7 @@ angular.module('pjcWordMultiSearch.search', [
       switch(lang){
         case 'eng': url='http://www.wordreference.com/definition/' + encodeURIComponent(search); break;
         case 'epo': url='http://www.simplavortaro.org/?vorto=' + encodeURIComponent(search); break;
+        case 'fra': url='http://dictionnaire.sensagent.com/{search}/fr-fr/'.replace('{search}', encodeURIComponent(search)); break;
         case 'spa': url='http://www.wordreference.com/definicion/' + encodeURIComponent(search); break;
       }
       return $sce.trustAsResourceUrl(url);
@@ -79,14 +81,14 @@ angular.module('pjcWordMultiSearch.search', [
 
     var isSupported = function(tab, lang){
       var langSupport = {
-        'rhinospike': 'eng epo spa',
-        'forvo': 'eng epo spa',
-        'tatoeba': 'eng epo spa',
-        'linguee': 'spa',
-        'googleimages': 'eng spa',
-        'googletranslate': 'epo spa',
-        'transdict': 'epo spa',
-        'monodict': 'eng epo spa'
+        'rhinospike': 'eng epo fra spa',
+        'forvo': 'eng epo fra spa',
+        'tatoeba': 'eng epo fra spa',
+        'linguee': 'fra spa',
+        'googleimages': 'eng fra spa',
+        'googletranslate': 'epo fra spa',
+        'transdict': 'epo fra spa',
+        'monodict': 'eng epo fra spa'
       };
       return langSupport[tab].indexOf(lang) > -1;
     }
