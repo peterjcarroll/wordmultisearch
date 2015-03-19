dc584f5c02422b15fdd50a60ac65c5c22aa76795
Peter Carroll <peter@eringal.com>
2015-03-19 14:48:50 -0500

    Added Romanian. Closes #9

--- app/search/search.js
+++ app/search/search.js
@@ -15,7 +15,7 @@ angular.module('pjcWordMultiSearch.search', [
 .controller('SearchPageController', function ($scope, $sce, $cookies, $anchorScroll, $location, $window) {
 
     var getRhinoSpikeLink = function(lang, search) {
-      var langCode = {'cmn': '13', 'deu': '3', 'eng': '1', 'epo': '22', 'fra': '6', 'jpn': '4', 'spa': '2'};
+      var langCode = {'cmn': '13', 'deu': '3', 'eng': '1', 'epo': '22', 'fra': '6', 'jpn': '4', 'ron': '21', 'spa': '2'};
       var url = 'https://rhinospike.com/search/?language={lang}&q={search}'
         .replace('{search}', encodeURIComponent(search))
         .replace('{lang}', langCode[lang]);      
@@ -23,7 +23,7 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getForvoLink = function(lang, search) {
-      var langCode = {'cmn': 'zh', 'deu': 'de', 'eng': 'en', 'epo': 'eo', 'fra': 'fr', 'jpn': 'ja', 'spa': 'es'};
+      var langCode = {'cmn': 'zh', 'deu': 'de', 'eng': 'en', 'epo': 'eo', 'fra': 'fr', 'jpn': 'ja', 'ron': 'ro', 'spa': 'es'};
       var url = 'http://www.forvo.com/search-{lang}/{search}/'
         .replace('{search}', encodeURIComponent(search))
         .replace('{lang}', langCode[lang]);
@@ -37,7 +37,7 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getLingueeLink = function(lang, search) {
-      var langCode = {'cmn': 'english-chinese', 'deu': 'english-german', 'fra': 'english-french', 'jpn': 'english-japanese', 'spa': 'english-spanish'};
+      var langCode = {'cmn': 'english-chinese', 'deu': 'english-german', 'fra': 'english-french', 'jpn': 'english-japanese', 'ron': 'english-romanian', 'spa': 'english-spanish'};
       var url = 'http://www.linguee.com/{lang}/search?source=auto&query={search}'
         .replace('{search}', encodeURIComponent(search))
         .replace('{lang}', langCode[lang]);
@@ -45,7 +45,7 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getGoogleImagesLink = function(lang, search) {
-      var langCode = {'cmn': 'com.hk', 'deu': 'de',  'eng': 'com', 'fra': 'fr', 'jpn': 'jp', 'spa': 'es'};
+      var langCode = {'cmn': 'com.hk', 'deu': 'de',  'eng': 'com', 'fra': 'fr', 'jpn': 'jp', 'ron': 'ro', 'spa': 'es'};
       //var url = 'ba-simple-proxy.php?mode=native&send_cookies=0&url=https%3A%2F%2Fwww.google.{lang}%2Fsearch%3Fq%3D{search}%26tbm%3Disch%26sout%3D1%26biw%3D1266%26bih%3D1442%26dpr%3D1'
       var url = 'https://www.google.{lang}/search?q={search}&tbm=isch&sout=1&biw=1266&bih=1442&dpr=1'
         .replace('{search}', encodeURIComponent(search))
@@ -54,7 +54,7 @@ angular.module('pjcWordMultiSearch.search', [
     };
 
     var getGoogleTranslateLink = function(lang, search) {
-      var langCode = {'cmn': 'zh-CN', 'deu': 'de', 'epo': 'eo', 'fra': 'fr', 'jpn': 'ja', 'spa': 'es'};
+      var langCode = {'cmn': 'zh-CN', 'deu': 'de', 'epo': 'eo', 'fra': 'fr', 'jpn': 'ja', 'ron': 'ro', 'spa': 'es'};
       //var url = 'ba-simple-proxy.php?mode=native&send_cookies=0&url=http%3A%2F%2Ftranslate.google.com%2F%3Fie%3DUTF-8%26sl%3D{lang}%26tl%3Den%26text%3D{search}'
       var url = 'http://translate.google.com/?ie=UTF-8&sl={lang}&tl=en&text={search}'
         .replace('{search}', encodeURIComponent(search))
@@ -70,6 +70,7 @@ angular.module('pjcWordMultiSearch.search', [
         case 'epo': url='http://lernu.net/cgi-bin/serchi.pl?delingvo=eo&allingvo=en&modelo=' + encodeURIComponent(search); break;
         case 'fra': url='http://www.wordreference.com/fren/' + encodeURIComponent(search); break;
         case 'jpn': url='http://jisho.org/words?eng=&dict=edict&jap=' + encodeURIComponent(search); break;
+        case 'ron': url='http://www.dictionarromanenglez.ro/en/?cuvant=' + encodeURIComponent(search); break;
         case 'spa': url='http://www.wordreference.com/es/en/translation.asp?spen=' + encodeURIComponent(search); break;
       }
       return $sce.trustAsResourceUrl(url);
@@ -84,6 +85,7 @@ angular.module('pjcWordMultiSearch.search', [
         case 'epo': url='http://www.simplavortaro.org/?vorto=' + encodeURIComponent(search); break;
         case 'fra': url='http://dictionnaire.sensagent.com/{search}/fr-fr/'.replace('{search}', encodeURIComponent(search)); break;
         case 'jpn': url='http://dictionary.goo.ne.jp/srch/all/{search}/m0u/'.replace('{search}', encodeURIComponent(search)); break;
+        case 'ron': url='http://dexonline.ro/definitie/' + encodeURIComponent(search); break;
         case 'spa': url='http://www.wordreference.com/definicion/' + encodeURIComponent(search); break;
       }
       return $sce.trustAsResourceUrl(url);
@@ -91,14 +93,14 @@ angular.module('pjcWordMultiSearch.search', [
 
     var isSupported = function(tab, lang){
       var langSupport = {
-        'rhinospike': 'cmn deu eng epo fra jpn spa',
-        'forvo': 'cmn deu eng epo fra jpn spa',
-        'tatoeba': 'cmn deu eng epo fra jpn spa',
-        'linguee': 'cmn deu fra jpn spa',
-        'googleimages': 'cmn deu eng fra jpn spa',
-        'googletranslate': 'cmn deu epo fra jpn spa',
-        'transdict': 'cmn deu epo fra jpn spa',
-        'monodict': 'cmn deu eng epo fra jpn spa'
+        'rhinospike': 'cmn deu eng epo fra jpn ron spa',
+        'forvo': 'cmn deu eng epo fra jpn ron spa',
+        'tatoeba': 'cmn deu eng epo fra jp ron spa',
+        'linguee': 'cmn deu fra jpn ron spa',
+        'googleimages': 'cmn deu eng fra jpn ron spa',
+        'googletranslate': 'cmn deu epo fra jpn ron spa',
+        'transdict': 'cmn deu epo fra jpn ron spa',
+        'monodict': 'cmn deu eng epo fra jpn ron spa'
       };
       return langSupport[tab].indexOf(lang) > -1;
     }
@@ -131,6 +133,7 @@ angular.module('pjcWordMultiSearch.search', [
       { code: 'epo', name:'Esperanto'},
       { code: 'fra', name:'Français'},
       { code: 'jpn', name:'日本語 (Japanese)'},
+      { code: 'ron', name:'Română'},
       { code: 'spa', name:'Español'},
     ];
 
